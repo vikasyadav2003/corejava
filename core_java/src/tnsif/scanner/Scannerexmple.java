841b294ec55c5a7e3d12dcb7384fd253381f20b9
lenovo <lenovo@DESKTOP-RC9JLQN>
2024-07-07 22:25:00 +0530

    first commit

---
+++ core_java/src/tnsif/scanner/Scannerexmple.java
@@ -0,0 +1,12 @@
+package tnsif.scanner;
+
+import java.util.Scanner;
+
+public class Scannerexmple {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int a=10;
+	System.out.println(a);
+}
+
+}
