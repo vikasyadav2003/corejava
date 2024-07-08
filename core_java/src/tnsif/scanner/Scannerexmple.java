fa9ae0bb87a2f7b1324217bae576094d28eb7fb5
lenovo <lenovo@DESKTOP-RC9JLQN.Dlink>
2024-07-08 15:58:44 +0530

    commit

--- core_java/src/tnsif/scanner/Scannerexmple.java
+++ core_java/src/tnsif/scanner/Scannerexmple.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Scannerexmple {
 public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
-	int a=10;
+	int a=sc.nextInt();
 	System.out.println(a);
 }
 
